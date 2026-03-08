09d72151ed6e12919628e70d60e2826d9810fa1c
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-03-09 01:20:43 +0600

    2.sql

--- Lab/2.sql
+++ Lab/2.sql
@@ -4,7 +4,6 @@ Setting environment for using XAMPP for Windows.
 Welcome to the MariaDB monitor.  Commands end with ; or \g.
 Your MariaDB connection id is 8
 Server version: 10.4.32-MariaDB mariadb.org binary distribution
-
 Copyright (c) 2000, 2018, Oracle, MariaDB Corporation Ab and others.
 
 Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
