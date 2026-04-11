31bdb6918ffc959722c6702f730ce466219a0ff1
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-12 00:01:46 +0600

    1-Dimensional Modeling.sql

---
+++ Exercise/1-Dimensional Modeling.sql
@@ -0,0 +1,15 @@
+-- Create a dimension table called route that will hold the route information.
+CREATE TABLE route (
+    route_id SERIAL PRIMARY KEY,
+    route_name VARCHAR(160),
+    park_name VARCHAR(160),
+    city_name VARCHAR(160)
+);
+
+-- Create a dimension table called week that will hold the week information.
+CREATE TABLE week (
+    week_id SERIAL PRIMARY KEY,
+    week INT,
+    month VARCHAR(160),
+    year INT
+);
