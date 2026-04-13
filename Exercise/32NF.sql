a2e0e248e8554556d3f4d5e4f0c178eebb4a4db5
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-13 22:15:18 +0600

    3.2NF.sql

---
+++ Exercise/32NF.sql
@@ -0,0 +1,34 @@
+/* INSTRUCTIONS:
+1. Create a new table 'cars' to hold car-specific attributes to satisfy 2NF.
+2. Insert distinct car records from 'customer_rentals' into the 'cars' table.
+3. Drop car-specific columns from 'customer_rentals' to remove partial dependencies.
+*/
+
+-- Create a new table to satisfy 2NF
+CREATE TABLE cars (
+  car_id VARCHAR(256) NULL,
+  model VARCHAR(128),
+  manufacturer VARCHAR(128),
+  type_car VARCHAR(128),
+  condition VARCHAR(128),
+  color VARCHAR(128)
+);
+
+-- Insert data into the new table
+INSERT INTO cars
+SELECT DISTINCT
+  car_id,
+  model,
+  manufacturer,
+  type_car,
+  condition,
+  color
+FROM customer_rentals;
+
+-- Drop columns in customer_rentals to satisfy 2NF
+ALTER TABLE customer_rentals
+DROP COLUMN model,
+DROP COLUMN manufacturer, 
+DROP COLUMN type_car,
+DROP COLUMN condition,
+DROP COLUMN color;
