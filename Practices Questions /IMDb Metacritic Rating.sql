eb24dd34c2d9dca75562a27d9d1bdbef6adfa812
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-22 23:59:36 +0600

    IMDb Metacritic Rating.sql

---
+++ Practices Questions /IMDb Metacritic Rating.sql
@@ -0,0 +1,9 @@
+-- Print the title and ratings of the movies released in 2012 
+-- whose Metacritic rating is more than 60 
+-- and Domestic collections exceed 10 Crores.
+
+SELECT title, rating
+FROM movies
+WHERE year = 2012
+  AND metacritic > 60
+  AND domestic_collection > 100000000;
