15973710569bf6066facb1bb2015bce48cd097a3
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-05-10 23:26:54 +0600

    IMDb Rating.sql

--- Practices Questions /IMDb Rating.sql
+++ Practices Questions /IMDb Rating.sql
@@ -1,6 +1,6 @@
 //Retrieve the title and rating of movies from the IMDb dataset 
 //that were released in 2014, belong to a genre starting with 'C', 
--- and had a budget exceeding 4 Crore.
+//and had a budget exceeding 4 Crore.
 
 SELECT title, rating
 FROM movies
