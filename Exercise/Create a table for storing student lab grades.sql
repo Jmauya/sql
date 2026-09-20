0e7feff53079684810a777ff99ecd89154b5e378
Jannatul Mauya <jannatulmauyanabila@gmail.com>
2026-09-20 22:59:03 +0600

    Create a table for storing student lab grades.sql

---
+++ Exercise/Create a table for storing student lab grades.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Lab_grades (
+student_id CHAR(4),
+name VARCHAR(30),
+major CHAR(3),
+section CHAR(1),
+days_present INT,
+project_marks DOUBLE,
+cgpa DECIMAL(3,2),
+submission_date DATE
+);
